5c1dee808a0d8457d65721942ad01b529540faa1
YOUR NAME <jhanov@gmail.com>
2022-01-18 20:28:53 -0600

    add create flower to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="home">
     <div class="container">
-        <h1>{{ message }}</h1>
+      <h1> Add a Flower </h1>
+      <div>
+        <input placeholder="Name" type="text" v-model="newFlowerParams.name" /> 
+        <input  placeholder="Family" type="text" v-model="newFlowerParams.family" />
+        <input placeholder="Color" type="text" v-model="newFlowerParams.color" />
+        <input placeholder="Image Url" type="text" v-model="newFlowerParams.image" />
+        <button v-on:click = "createFlower()">  Create Flower </button>
+        
+
+      </div>
+      <h1>{{ message }}</h1>
       <div class="flex-container">
         <div v-for="flower in flowers" class="card" style="width: 18rem;">
         <img class="card-img-top" v-bind:src="flower.image" alt="Card image cap">
@@ -45,6 +55,7 @@ export default {
   data: function () {
     return {
       flowers: [],
+      newFlowerParams: {},
       message: "The Flower App",
     };
   },
@@ -52,6 +63,18 @@ export default {
     this.indexFlowers();
   },
   methods: {
+    createFlower: function () {
+      axios
+        .post("/flowers", this.newFlowerParams)
+        .then((response) => {
+          console.log("flowers create", response);
+          this.flowers.push(response.data);
+          this.newFlowerParams = {};
+        })
+        .catch((error) => {
+          console.log("Flower error", error.response);
+        });
+    },
     indexFlowers: function () {
       axios.get("/flowers").then((response) => {
         console.log("flowers index", response);
